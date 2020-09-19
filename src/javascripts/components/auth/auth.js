67157218ef6ef56c83be6dd2136c3c825ac12773
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-09-19 17:54:54 -0500

    styling on app

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -2,13 +2,17 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const signMeIn = () => {
-  $('#app').html('');
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider);
 };
 
 const loginButton = () => {
-  const domString = '<div id="auth"><h4>Welcome to Pasture</h4><button id="google-auth" class="btn btn-primary btn-lg"><i class="fab fa-google"></i></i>oogle Login</button></div>';
+  const domString = `<div id="auth">
+                      <h4>Welcome to Pasture</h4>
+                      <button id="google-auth" class="btn btn-primary btn-lg">
+                        <i class="fab fa-google"></i></i>oogle Login
+                      </button>
+                    </div>`;
 
   $('#app').html(domString);
   $('#google-auth').on('click', signMeIn);
--- src/javascripts/components/myNavbar/myNavbar.js
+++ src/javascripts/components/myNavbar/myNavbar.js
@@ -14,19 +14,19 @@ const logoutEvent = () => {
 const myNavbar = () => {
   $('#nav').html(
     `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Pasture</a>
+      <a class="navbar-brand" href="#">Pasture</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <button class="nav-link btn btn-danger" id="navbar-logout-button">Logout</button>
-        </li>
-      </ul>
-    </div>
-  </nav>`
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <button class="nav-link btn btn-danger" id="navbar-logout-button">Logout</button>
+          </li>
+        </ul>
+      </div>
+    </nav>`
   );
 
   logoutEvent();
