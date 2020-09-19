1f925606e6a7db53d6a108b411a26ca6786b195c
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-09-19 14:41:12 -0500

    added user request

--- src/javascripts/helpers/data/userData.js
+++ src/javascripts/helpers/data/userData.js
@@ -4,21 +4,18 @@ import apiKeys from '../apiKeys.json';
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const getUser = (userObj) => {
-  axios.get(`${baseUrl}/farmers.json?orderBy="uid"&equalTo="${userObj.uid}"`)
+  axios
+    .get(`${baseUrl}/farmers.json?orderBy="uid"&equalTo="${userObj.uid}"`)
     .then((resp) => {
       if (Object.values(resp.data).length === 0) {
-        // POST OBJECT TO FARMERS
-        // {
-        //   image:
-        //   uid:
-        //   displayName:
-        //   email:
-        //   lastSignInTime:
-        // }
-
-        // use axios.post
-        // pass it the url ${baseUrl}/farmers.json
-        // pass it an object
+        const user = {
+          image: userObj.photoUrl,
+          uid: userObj.uid,
+          name: userObj.displayName,
+          email: userObj.email,
+          lastSignInTime: userObj.lastSignInTime,
+        };
+        axios.post(`${baseUrl}/farmers.json`, user);
       } else {
         console.warn('User Exists');
       }
