0d6fd9eea300eb1293327a8caac83ab59148114c
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-09-19 22:31:49 -0500

    removed pasture

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,6 +1,5 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import pasture from '../../components/pasture/pasture';
 import userData from './farmerData';
 import auth from '../../components/auth/auth';
 import myNavbar from '../../components/myNavbar/myNavbar';
@@ -11,7 +10,6 @@ const checkLoginStatus = () => {
       const currentFarmer = userData.setCurrentFarmer(farmer);
       console.warn(currentFarmer);
       myNavbar.myNavbar();
-      pasture.buildCows();
     } else {
       auth.loginButton();
       $('#nav').html('');
