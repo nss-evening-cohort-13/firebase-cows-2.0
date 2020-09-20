ea11729fb0f9f05048b175b8edc403fbd1e22a4d
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-09-20 09:31:45 -0500

    added styles

--- src/javascripts/helpers/data/cowData.js
+++ src/javascripts/helpers/data/cowData.js
@@ -3,6 +3,7 @@ import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
+// STUDENTS: Refactor this to use new Promise syntax
 const getCows = () => axios.get(`${baseUrl}/cows.json`);
 
 export default { getCows };
