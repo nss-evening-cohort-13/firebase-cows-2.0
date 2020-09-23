a2a6e120e9dab962cfab3c1986ab1e30ed643d00
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-09-22 21:34:39 -0500

    added delete

--- src/javascripts/helpers/data/cowData.js
+++ src/javascripts/helpers/data/cowData.js
@@ -18,4 +18,29 @@ const getAllCows = () => new Promise((resolve, reject) => {
 
 const deleteCow = (firebaseKey) => axios.delete(`${baseUrl}/cows/${firebaseKey}.json`);
 
-export default { getAllCows, deleteCow };
+const addCow = (data) => axios.post(`${baseUrl}/cows.json`, data)
+  .then((response) => {
+    const update = { firebaseKey: response.data.name };
+    axios.patch(`${baseUrl}/cows/${response.data.name}.json`, update);
+  }).catch((error) => console.warn(error));
+
+const getFarmerCows = (farmerUid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/cows.json?orderBy="farmerUid"&equalTo="${farmerUid}"`)
+    .then((response) => {
+      const farmerCows = response.data;
+      const cows = [];
+      if (farmerCows) {
+        Object.keys(farmerCows).forEach((cowId) => {
+          cows.push(farmerCows[cowId]);
+        });
+      }
+      resolve(cows);
+    }).catch((error) => reject(error));
+});
+
+export default {
+  getAllCows,
+  deleteCow,
+  addCow,
+  getFarmerCows
+};
